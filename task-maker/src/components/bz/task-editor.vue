e5a42821f0868eb51b0c11d46a716beed93e774e
qinggeng <qinggeng@gmail.com>
2017-08-01 06:45:04 +0800

    实现批量编辑列表展现

--- task-maker/src/components/bz/task-editor.vue
+++ task-maker/src/components/bz/task-editor.vue
@@ -15,15 +15,148 @@
       data-name='batch_task_edit_dialog'
       :dialogClass = 'dialogClass'
       >
+      <mu-stepper :activeStep='currentStep'>
+        <mu-step>
+          <mu-step-label>
+            编辑标题
+          </mu-step-label>
+        </mu-step>
+        <mu-step>
+          <mu-step-label>
+            编辑属性
+          </mu-step-label>
+        </mu-step>
+        <mu-step>
+          <mu-step-label>
+            完成
+          </mu-step-label>
+        </mu-step>
+      </mu-stepper>
+      <task-list :tasks  = 'tasks'/>
       <mu-flat-button label='完成' slot='actions' primary @click='commitCreation'/>
     </mu-dialog>
   </div>
 </template>
 
 <script>
+"use strict";
+const taskStr=`
+旧bug修复
+  1121
+  1134
+重整测评和训练分类
+  数据整理
+  工具实现
+  数据库实现
+`.trim();
+let j2s = JSON.stringify;
+
+let parseTask = (tstr) => {
+  let terminals = /^(\s*)([^\s].*)$/;
+  let match = terminals.exec(tstr);
+  let indent = match[1].replace('\t', '  ');
+  let title = match[2];
+  return {
+    indent,
+    title,
+    /* assigned: '',*/
+    /* priority: '',*/
+    /* severity: '',*/
+    /* deadline: '',*/
+    /* riskAssessment: '',*/
+  };
+};
+
+let generateTasks = (tasks, stack, task) =>
+{
+  let subTasks = [];
+  while(tasks.length > 0)
+  {
+    let cachedTask = tasks[tasks.length - 1];
+    
+    if (cachedTask.indent.length <= task.indent.length)
+    {
+      break;
+    }
+    subTasks.push(cachedTask);
+    tasks.pop();
+  }
+  if (subTasks.length > 0)
+  {
+    task.subTasks = subTasks;
+  }
+  tasks.push(task);
+  if (stack.length === 0)
+  {
+    return tasks.reverse();
+  }
+  return generateTasks(tasks, stack.slice(0, -1), stack[Math.max(0, stack.length - 1)]);
+}
+
+
+let parseTasks = (tstr) => {
+  let lines = tstr.split('\n');
+  let stack = [];
+  for (const line of lines)
+  {
+    let task = parseTask(line);
+    stack.push(task);
+  }
+  let tasks = 
+    generateTasks(
+      [], 
+      stack.slice(0, -1), 
+      stack[Math.max(0, stack.length - 1)]);
+  return tasks;
+};
+
+
+let taskData = parseTasks(taskStr);
+
 
 import basic_styles from '@/styles';
 import NewTaskDialog from "@/components/bz/dialogs/new-tasks-wizard"
+/**
+ *  任务列表组件
+ */
+let taskListTemplate = `
+  <mu-list>
+    <mu-list-item v-for="task of tasks" :title="task.title">
+      <div slot='describe' style="
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      ">
+        <span>负责人:</span>
+        <span>优先级:</span>
+        <span>严重程度:</span>
+        <span>截止日期:</span>
+      </div>
+      <task-list v-if='task.subTasks !== undefined' :tasks = 'task.subTasks'/>
+    </mu-list-item>
+  </mu-list>
+`.trim();
+let taskList = {
+  template: taskListTemplate,
+  name: 'task-list',
+  props: {
+    tasks: {
+      type: Array,
+      require: true,
+    },
+  },
+  data: function()
+  {
+    return {};
+  },
+  computed: {},
+};
+/**
+ *
+ *  任务批量编辑向导组件
+ *
+ */
 const default_styles = {
   editor_frame: {
     ...basic_styles.v_container,
@@ -69,6 +202,7 @@ export default {
       default_styles: {...default_styles},
       currentState: {},
       dialogClass: 'dialog',
+      tasks: taskData,
     };
   },// end of data
   computed: {
@@ -80,6 +214,10 @@ export default {
     {
       return true;
     },
+    currentStep: function()
+    {
+      return 1;
+    },
   },// end of computed
   methods: {
     batch_add_task: function() 
@@ -90,6 +228,9 @@ export default {
     {
     },
   },// end of methods
+  components: {
+    'task-list': taskList,
+  },//end of components
 }
 </script>
 
